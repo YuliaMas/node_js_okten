8d86f6d845003d9375717c7fdefc470f7249ab66
Yuliya Masechko <yulia.masechko@gmail.com>
2024-10-07 01:16:00 +0300

    logout

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -8,42 +8,42 @@ import { EmailTypeEnum } from "../enums/email-type.enum";
 import { EmailTypeToPayload } from "../types/email-type-to-payload.type";
 
 class EmailService {
-    private transporter: Transporter;
+  private transporter: Transporter;
 
-    constructor() {
-        this.transporter = nodemailer.createTransport({
-            service: "gmail",
-            from: "No reply",
-            auth: {
-                user: configs.SMTP_EMAIL,
-                pass: configs.SMTP_PASSWORD,
-            },
-        });
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: "gmail",
+      from: "No reply",
+      auth: {
+        user: configs.SMTP_EMAIL,
+        pass: configs.SMTP_PASSWORD,
+      },
+    });
 
-        const hbsOptions = {
-            viewEngine: {
-                extname: ".hbs",
-                defaultLayout: "main",
-                layoutsDir: path.join(process.cwd(), "src", "templates", "layouts"),
-                partialsDir: path.join(process.cwd(), "src", "templates", "partials"),
-            },
-            viewPath: path.join(process.cwd(), "src", "templates", "views"),
-            extName: ".hbs",
-        };
+    const hbsOptions = {
+      viewEngine: {
+        extname: ".hbs",
+        defaultLayout: "main",
+        layoutsDir: path.join(process.cwd(), "src", "templates", "layouts"),
+        partialsDir: path.join(process.cwd(), "src", "templates", "partials"),
+      },
+      viewPath: path.join(process.cwd(), "src", "templates", "views"),
+      extName: ".hbs",
+    };
 
-        this.transporter.use("compile", hbs(hbsOptions));
-    }
+    this.transporter.use("compile", hbs(hbsOptions));
+  }
 
-    public async sendMail<T extends EmailTypeEnum>(
-        type: T,
-        to: string,
-        context: EmailTypeToPayload[T],
-    ): Promise<void> {
-        const { subject, template } = emailConstants[type];
-
-        const options = { to, subject, template, context };
-        await this.transporter.sendMail(options);
-    }
+  public async sendMail<T extends EmailTypeEnum>(
+    type: T,
+    to: string,
+    context: EmailTypeToPayload[T],
+  ): Promise<void> {
+    const { subject, template } = emailConstants[type];
+    context["frontUrl"] = configs.APP_FRONT_URL;
+    const options = { to, subject, template, context };
+    await this.transporter.sendMail(options);
+  }
 }
 
 export const emailService = new EmailService();
