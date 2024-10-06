36dfb0c0d77769cec86e808caf343b0bc1ad594b
Yuliya Masechko <yulia.masechko@gmail.com>
2024-10-07 01:12:45 +0300

    logout

--- src/types/email-type-to-payload.type.ts
+++ src/types/email-type-to-payload.type.ts
@@ -3,13 +3,13 @@ import { EmailPayloadCombined } from "./email-payload-combined.type";
 import { PickRequired } from "./pick-required.type";
 
 export type EmailTypeToPayload = {
-    [EmailTypeEnum.WELCOME]: PickRequired<EmailPayloadCombined, "name">;
+  [EmailTypeEnum.WELCOME]: PickRequired<EmailPayloadCombined, "name">;
 
-    [EmailTypeEnum.FORGOT_PASSWORD]: PickRequired<
-        EmailPayloadCombined,
-        "name" | "email"
-    >;
+  [EmailTypeEnum.FORGOT_PASSWORD]: PickRequired<
+    EmailPayloadCombined,
+    "name" | "email"
+  >;
 
-    [EmailTypeEnum.OLD_VISIT]: PickRequired<EmailPayloadCombined, "email">;
-    [EmailTypeEnum.LOGOUT]: PickRequired<EmailPayloadCombined, "name">;
-}
+  [EmailTypeEnum.OLD_VISIT]: PickRequired<EmailPayloadCombined, "email">;
+  [EmailTypeEnum.LOGOUT]: PickRequired<EmailPayloadCombined, "name">;
+};
