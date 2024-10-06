40e28d79fd67aacb5304b64db4a22dd0691c88e1
Yuliya Masechko <yulia.masechko@gmail.com>
2024-10-07 01:10:03 +0300

    logout

--- src/enums/email-type.enum.ts
+++ src/enums/email-type.enum.ts
@@ -1,5 +1,6 @@
 export enum EmailTypeEnum {
-    WELCOME = "welcome",
-    FORGOT_PASSWORD = "forgot-password",
-    OLD_VISIT = "old-visit",
+  WELCOME = "welcome",
+  FORGOT_PASSWORD = "forgot-password",
+  OLD_VISIT = "old-visit",
+  LOGOUT = "logout",
 }
